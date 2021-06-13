8056dae4cb2b5685c4bf964a2a2fbd73b09a5894
Nihal A <nihalavulan1@gmail.com>
2021-06-13 15:58:52 +0530

    Testimonials issue fixed

--- js/main.js
+++ js/main.js
@@ -13,6 +13,19 @@ function showHideMenu() {
     menu.style.display = "none";
   }
 }
+$(document).ready(function () {
+  $("#testimonial-slider").owlCarousel({
+    items: 3,
+    itemsDesktop: [1000, 3],
+    itemsDesktopSmall: [980, 2],
+    itemsTablet: [768, 2],
+    itemsMobile: [650, 1],
+    pagination: true,
+    navigation: false,
+    slideSpeed: 1000,
+    autoPlay: true,
+  });
+});
 $("#submit-form").submit((e) => {
   e.preventDefault();
   $.ajax({
